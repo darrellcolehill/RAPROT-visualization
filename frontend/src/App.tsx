79a2eb5801880f3e8d310551e387c52bbe887111
Darrell Cole Hill <darrellcolehill@gmail.com>
2024-07-15 10:56:25 -0400

    Shows text on right, tree on left

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,111 +1,147 @@
 /* eslint-disable @typescript-eslint/no-extra-non-null-assertion */
-import Node from './Node';
-import data from '../public/EXO_FULL.json';
+import React, { useState, useEffect } from 'react';
+import Tree from 'react-d3-tree';
+import data from './EXO_FULL.json';
+import { Grid } from '@mui/material';
 
 const App = () => {
-
   type BackendNode = {
     children: number[]
     text: string
     index: number
   }
 
+  interface RawNodeDatum {
+    name: string;
+    attributes?: Record<string, string | number | boolean>;
+    children?: RawNodeDatum[];
+  }
 
-  function mapLayersToArray(): BackendNode[][] {
-
+  const mapLayersToArray = (): BackendNode[][] => {
     const layers = Object.keys(data).length;
-
     const layerAcc: BackendNode[][] = [];
 
-    for(let i = 0; i < layers; i++) {
+    for (let i = 0; i < layers; i++) {
       layerAcc[i] = data['layer_' + i];
     }
 
     return layerAcc;
   }
 
-
-  function flatMapLayers(): BackendNode[] {
+  const flatMapLayers = (): BackendNode[] => {
     let acc: BackendNode[] = [];
     const layers = Object.keys(data).length;
 
-    for(let i = 0; i < layers; i++) {
+    for (let i = 0; i < layers; i++) {
       acc = [...acc, ...data['layer_' + i]]
     }
     return acc;
   }
 
-  type UINode = {
-    children?: UINode[]
-    text: string
-    index: string
+  const flatLayers = flatMapLayers();
+
+  const getNode = (id: number): BackendNode | undefined => {
+    return flatLayers.find((a) => a.index === id);
   }
 
-
-  const flatLayers = flatMapLayers()
-
-  function getNode(id: number): BackendNode | undefined {
-    return flatLayers.find((a) => a.index == id)
-  }
-
-  function mapData() {
-    const arrData = mapLayersToArray()
-    for(let i = 0; i < arrData.length; i++) {
-      arrData[i].sort((a, b) => ((-1*a.index) - (-1*b.index)));
-    }
-    console.log(flatLayers)
-
-    const res = []
-    for(let i = 0; i < arrData[arrData.length - 1].length; i++) {
-      res.push(_mapdata(arrData[arrData.length - 1], arrData[arrData.length - 1][i].index))
-    }
-    return res;
-  }
-
-
-  let totalNodesProcessed = 0;
-  function _mapdata(arrData: BackendNode[], nodeIdx: number): UINode {
-    const curNode = getNode(nodeIdx)!!
-    if(curNode.children.length === 0 ) {
-      const t: UINode = {
-        text: curNode.text,
-        index: `${curNode.index}`,
+  const _mapdata = (arrData: BackendNode[], nodeIdx: number): RawNodeDatum => {
+    const curNode = getNode(nodeIdx)!!;
+    if (curNode.children.length === 0) {
+      const t: RawNodeDatum= {
+        attributes: { text: curNode.text },
+        name: `${curNode.index}`,
         children: []
-      }
-      totalNodesProcessed++;
+      };
       return t;
     }
 
-    const children = []
-    for(let i = 0; i < curNode.children.length; i++) {
-      const child = _mapdata(arrData, curNode.children[i]);
-      children.push(child)
-    }
-
-    const t = {
-      text: curNode.text,
-      index: `${curNode.index}`,
+    const children = curNode.children.map((childIdx) => _mapdata(arrData, childIdx));
+    
+    const t: RawNodeDatum = {
+      attributes: { text: curNode.text },
+      name: `${curNode.index}`,
       children: children
-    }
-    totalNodesProcessed++;
+    };
     return t;
   }
 
+  const mapData = (): RawNodeDatum => {
+    console.log("running map data")
+    const arrData = mapLayersToArray();
+    arrData.forEach(layer => layer.sort((a, b) => b.index - a.index));
 
-  console.log(mapData())
-  // TODO NOTE: for some reason, this is 521, and not 520
-  console.log(totalNodesProcessed)
+    const mappedRootNodes = arrData[arrData.length - 1].map(node => _mapdata(arrData[arrData.length - 1], node.index));
+    const root: RawNodeDatum = {
+      name: `root`,
+      children: mappedRootNodes
+    };
+    return root;
+  }
+
+  const [treeData, setTreeData] = useState<RawNodeDatum>();
+  const [selectedNodeText, setSelectedNodeText] = useState<string>()
+
+  useEffect(() => {
+    const mappedData = mapData();
+    setTreeData(mappedData);
+  }, []);
 
 
+  useEffect(() => {
+    console.log("tree data updated")
+    console.log(treeData)
+  }, [treeData])
+
+
+  const handleNodeClick = (nodeDatum: RawNodeDatum, evt: React.MouseEvent) => {
+    console.log('Node clicked:', nodeDatum);
+    const text = nodeDatum?.attributes?.text
+    if(text) {
+      console.log("Setting text: " + text)
+      setSelectedNodeText(`${text}`)
+    }
+  };
+
+  const renderCustomNode = ({ nodeDatum, toggleNode }: { nodeDatum: RawNodeDatum; toggleNode: () => void }) => (
+    <g>
+      <circle
+        r={15}
+        fill="lightblue"
+        onClick={(evt) => {
+          handleNodeClick(nodeDatum, evt);
+          toggleNode();
+        }}
+      />
+      <text fill="black" x="20" dy=".35em">
+        {nodeDatum.name}
+      </text>
+    </g>
+  );
 
   return (
-    <div className="App">
-          {data["layer_" + (Object.keys(data).length - 1)].map(node => (
-            <Node key={node.index} node={node} layers={data} layerIdx={3}/>
-          ))}
+    <div style={{ height: '100vh', width: '100vw' }}>
+      <Grid container style={{ height: '100%', width: '100%', background:"white" }}>
+        <Grid item xs={6} style={{ height: '100%' }}>
+          {treeData ? (
+            <Tree
+              data={treeData}
+              rootNodeClassName="node__root"
+              branchNodeClassName="node__branch"
+              leafNodeClassName="node__leaf"
+              initialDepth={1}
+              renderCustomNodeElement={renderCustomNode}
+            />
+          ) : (
+            <></>
+          )}
+        </Grid>
+        <Grid item xs={6} style={{ height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', paddingRight:"50px" }}>
+          {selectedNodeText ? <p style={{ color: "black" }}>{selectedNodeText}</p> : <></>}
+        </Grid>
+      </Grid>
     </div>
   );
 };
 
-export default App;
 
+export default App;
